590005e12f6d5b7fbb1cc4589856b686e6f9d0ba
Billy Walton <BillyW@magenic.com>
2021-05-30 01:32:15 -0500

    Take Matthew Renze's repo and convert it to ASP.NET Core 3.1

---
+++ src/DDD/DDD/Products/Controllers/ProductsController.cs
@@ -0,0 +1,29 @@
+﻿using DDD.Products.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DDD.Products.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly List<Product> _products = new List<Product>
+        {
+            new Product { Id = 1, Name = "Ice Cream", Price = 1.23m },
+            new Product { Id = 2, Name = "Cake", Price = 2.34m }
+        };
+
+        public IActionResult Index()
+        {
+            return View(_products);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var product = _products
+                .First(p => p.Id == id);
+
+            return View(product);
+        }
+    }
+}
